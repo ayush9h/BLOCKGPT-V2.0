8f1d314a7745fa3d0d6ced8f52f78ae47ad9de88
Ayush Kumar <ayush.kumar.21cse@bmu.edu.in>
2025-03-22 13:50:12 +0530

    added next-auth

--- client/src/app/components/landing.tsx
+++ client/src/app/components/landing.tsx
@@ -1,9 +1,20 @@
+import { signIn } from "../auth"
+
 export default function Landing(){
     return(
         <>
             <div className="max-w-7xl m-auto flex flex-col items-center justify-center text-center">
                 <h1 className="font-display text-8xl font-semibold text-blue-500">BLOCKGPT</h1>
             </div>
+
+            <form
+              action={async () => {
+                "use server"
+                await signIn("google")
+              }}
+            >
+              <button type="submit">Signin with Google</button>
+            </form>
         </>
     )
 }
