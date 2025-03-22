b62f131b59c44e6804dd2f7a55662254c3df3048
Ayush Kumar <ayush.kumar.21cse@bmu.edu.in>
2025-03-22 14:37:24 +0530

    added googleoauth and githuboauth

--- client/src/app/components/landing.tsx
+++ client/src/app/components/landing.tsx
@@ -1,20 +1,37 @@
 import { signIn } from "../auth"
 
-export default function Landing(){
-    return(
-        <>
-            <div className="max-w-7xl m-auto flex flex-col items-center justify-center text-center">
-                <h1 className="font-display text-8xl font-semibold text-blue-500">BLOCKGPT</h1>
+export default function Landing() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <div className="text-center mb-8">
+                <h1 className="font-header text-8xl font-semibold text-blue-500">BLOCKGPT</h1>
             </div>
 
-            <form
-              action={async () => {
-                "use server"
-                await signIn("google")
-              }}
-            >
-              <button type="submit">Signin with Google</button>
-            </form>
-        </>
+            <div className="flex gap-4">
+                <form
+                    action={async () => {
+                        "use server"
+                        await signIn("google")
+                    }}
+                >
+                    <button className="text-slate-800 px-5 py-2.5 gap-2 font-medium flex flex-row justify-center items-center text-center bg-slate-200 rounded-md border border-slate-300 hover:bg-slate-300 duration-150 ease-in-out cursor-pointer">
+                        <img className="w-5 h-5" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo" />
+                        <span>Login with Google</span>
+                    </button>
+                </form>
+
+                <form
+                    action={async () => {
+                        "use server"
+                        await signIn("github")
+                    }}
+                >
+                    <button className="text-slate-800 px-5 py-2.5 gap-2 font-medium flex flex-row justify-center items-center text-center bg-slate-200 rounded-md border border-slate-300 hover:bg-slate-300 duration-150 ease-in-out cursor-pointer">
+                    <img className="w-5 h-5" src="https://www.svgrepo.com/show/512317/github-142.svg" loading="lazy" alt="github logo"></img>
+                    Sign in with Github
+                    </button>
+                </form>
+            </div>
+        </div>
     )
 }
