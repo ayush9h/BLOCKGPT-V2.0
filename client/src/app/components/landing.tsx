e515d452f3ea4ab0c9499373afbb8be8d9c1bfab
Ayush Kumar <ayush.kumar.21cse@bmu.edu.in>
2025-03-22 10:02:32 +0530

    added next-auth setup

---
+++ client/src/app/components/landing.tsx
@@ -0,0 +1,9 @@
+export default function Landing(){
+    return(
+        <>
+            <div className="max-w-7xl m-auto flex flex-col items-center justify-center text-center">
+                <h1 className="font-display text-8xl font-semibold text-blue-500">BLOCKGPT</h1>
+            </div>
+        </>
+    )
+}
