cdb87b1cf7b373cdb449227a1cc2237c50c875a7
Ayush Kumar <ayush.kumar.21cse@bmu.edu.in>
2025-07-13 16:08:17 +0530

    fixed the temperature

--- server/app/utils/models.py
+++ server/app/utils/models.py
@@ -7,29 +7,26 @@ config_key = Config.GROQ_API_KEY
 
 class LLMModel:
 
-    def __init__(self, temperature):
-        self.temperature = temperature
-
-    def llama_model(self):
+    def llama_model():
         return ChatGroq(
             model="llama3-8b-8192",
-            temperature=self.temperature,
+            temperature=0.3,
             max_retries=2,
             api_key=config_key,
         )
 
-    def qwen_model(self):
+    def qwen_model():
         return ChatGroq(
             model="qwen/qwen3-32b",
-            temperature=self.temperature,
+            temperature=0.3,
             max_retries=2,
             api_key=config_key,
         )
 
-    def gemma_model(self):
+    def gemma_model():
         return ChatGroq(
             model="gemma2-9b-it",
-            temperature=self.temperature,
+            temperature=0.3,
             max_retries=2,
             api_key=config_key,
         )
