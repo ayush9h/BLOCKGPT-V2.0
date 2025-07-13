929aea778d0e08ecc71e514347a681d76c401c70
Ayush Kumar <ayush.kumar.21cse@bmu.edu.in>
2025-07-13 18:33:53 +0530

    fixed missing secret keyword from providers

--- client/src/app/auth.ts
+++ client/src/app/auth.ts
@@ -4,4 +4,5 @@ import GitHub from "next-auth/providers/github"
 
 export const {handlers, signIn, signOut, auth}=NextAuth({
     providers:[Google, GitHub],
+    secret:process.env.NEXTAUTH_SECRET
 })
